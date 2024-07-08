588b877b268f3276a12f3817d30df2c058f61f27
MMMcMahon <Mike.McMahon@dfo-mpo.gc.ca>
2024-07-08 18:34:33 +0000

    fix example

--- inst/exampleRun.R
+++ inst/exampleRun.R
@@ -1,5 +1,5 @@
 BFT_Data <- prepareData(username = "<yourusername>", password = "<yourpassword>", 
                         data.dir = "C:/Users/McMahonM/OneDrive - DFO-MPO/data/wrangled", 
-                        usepkg = 'roracle', update = F)
+                        usepkg = 'roracle',force.extract  = F)
 BFT_LOGS <- BFT_LOGS(BFT_Data)
 gearByYear(BFT_LOGS) %>% View()
