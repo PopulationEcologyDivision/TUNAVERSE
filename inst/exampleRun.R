d0f59bb46f6f8cdcbd67aef1339c2d018d63cf5d
MMMcMahon <Mike.McMahon@dfo-mpo.gc.ca>
2024-07-08 18:44:20 +0000

    exports gearByYear

--- inst/exampleRun.R
+++ inst/exampleRun.R
@@ -1,5 +1,8 @@
-BFT_Data <- prepareData(username = "<yourusername>", password = "<yourpassword>", 
+devtools::install_github('PopulationEcologyDivision/TUNAVERSE')
+library(TUNAVERSE)
+  BFT_Data <- prepareData(username = "<yourusername>", password = "<yourpassword>", 
                         data.dir = "C:/Users/McMahonM/OneDrive - DFO-MPO/data/wrangled", 
                         usepkg = 'roracle',force.extract  = F)
 BFT_LOGS <- BFT_LOGS(BFT_Data)
+library(dplyr)
 gearByYear(BFT_LOGS) %>% View()
