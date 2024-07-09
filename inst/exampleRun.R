98ba67693b8a9f50b2b6b11d9962b17f3bb9bdeb
MMMcMahon <Mike.McMahon@dfo-mpo.gc.ca>
2024-07-09 17:56:47 +0000

    adds dsn as a parameter to prepareData

--- inst/exampleRun.R
+++ inst/exampleRun.R
@@ -1,7 +1,7 @@
 devtools::install_github('PopulationEcologyDivision/TUNAVERSE')
 library(TUNAVERSE)
-  BFT_Data <- prepareData(username = "<yourusername>", password = "<yourpassword>", 
-                        data.dir = "C:/Users/McMahonM/OneDrive - DFO-MPO/data/wrangled", 
+BFT_Data <- prepareData(username = "<yourusername>", password = "<yourpassword>", 
+                        dsn="<yourdsn>", data.dir = "C:/Users/McMahonM/OneDrive - DFO-MPO/data/wrangled", 
                         usepkg = 'roracle',force.extract  = F)
 BFT_LOGS <- BFT_LOGS(BFT_Data)
 library(dplyr)
