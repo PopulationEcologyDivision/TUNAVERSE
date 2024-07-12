0ecd72f08447b13bd82b6adfd25a167929a0ba3f
Hanke <Alex.Hanke@dfo-mpo.gc.ca>
2024-07-12 13:57:02 -0300

    Added AREAS to alltables list Added all.x to join on line 85

--- R/metdata.R
+++ R/metdata.R
@@ -1,11 +1,11 @@
 #' Tables for the TUNA-VERSE package
 #'
-#' A character vector of table names used in the TUNA-VERSE package.
+#' A character vector of table names used in the TUNAVERSE package.
 #'
-#' @format A character vector with 11 elements - 
+#' @format A character vector with 12 elements - 
 #' MON_DOCS, TRIPS, PRO_SPC_INFO, LOG_EFRT_STD_INFO, LOG_SPC_STD_INFO, 
 #' SLIP_OFFLD_STD_INFO, SLIP_BUYR_STD_INFO, SLIP_SPC_STD_INFO, LOG_EFRT_ENTRD_DETS, 
-#' MON_DOC_ENTRD_DETS, LOG_SPC_ENTRD_DETS
+#' MON_DOC_ENTRD_DETS, LOG_SPC_ENTRD_DETS, AREAS
 #' @name allTables
 #' @docType data
 NULL
