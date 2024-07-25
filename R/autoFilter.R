ce1eca002601c6458909e8c46f0972d375444f97
MMMcMahon <Mike.McMahon@dfo-mpo.gc.ca>
2024-07-25 17:01:49 +0000

    modifies autofilter to use data.table and adds data.table as a dependency.  Turns out to be ~3x faster than merge()

--- R/autoFilter.R
+++ R/autoFilter.R
@@ -1,30 +1,39 @@
-autoFilter <- function(tblList, quiet = T){
+#' @title autoFilter
+#' @description This function operates on the various object identified in \code{tblList}, and 
+#' ensures that they don't contain extraneous records.  For example, if the TRIPS object within 
+#' tblList is filtered to only 2013 records, other objects such as MON_DOCS and LOG_SPC_STD_INFO 
+#' will be filtered to ensure that only contain records that related to the remianing trips.
+#' @param tblList  this is a vector of object names necessary for the functions of TUNAVERSE to run. 
+#' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
+#' @export
+autoFilter <- function(tblList){
+  # Convert data frames to data tables
+  tblList <- lapply(tblList, as.data.table)
   LOOPAGAIN = T
   while (LOOPAGAIN){
-    precnt = sum(sapply(tblList, NROW))
-    if (!quiet) print(precnt)
-    tblList$SLIP_OFFLD_STD_INFO <- merge(tblList$SLIP_OFFLD_STD_INFO,  unique(tblList$MON_DOCS[,"MON_DOC_ID",drop=F]))
-    tblList$SLIP_BUYR_STD_INFO  <- merge(tblList$SLIP_BUYR_STD_INFO, unique(tblList$SLIP_OFFLD_STD_INFO[,"SLIP_OFFLD_STD_INFO_ID",drop=F]))
-    tblList$SLIP_SPC_STD_INFO   <- merge(tblList$SLIP_SPC_STD_INFO,  unique(tblList$SLIP_BUYR_STD_INFO[,"SLIP_BUYR_STD_INFO_ID",drop=F]))  
+    precnt = sum(sapply(tblList, nrow))
+    tblList$SLIP_OFFLD_STD_INFO <- tblList$SLIP_OFFLD_STD_INFO[unique(tblList$MON_DOCS[, .(MON_DOC_ID)]), on = "MON_DOC_ID", nomatch = 0]
+    tblList$SLIP_BUYR_STD_INFO  <- tblList$SLIP_BUYR_STD_INFO[unique(tblList$SLIP_OFFLD_STD_INFO[, .(SLIP_OFFLD_STD_INFO_ID)]), on = "SLIP_OFFLD_STD_INFO_ID", nomatch = 0]
+    tblList$SLIP_SPC_STD_INFO   <- tblList$SLIP_SPC_STD_INFO[unique(tblList$SLIP_BUYR_STD_INFO[, .(SLIP_BUYR_STD_INFO_ID)]), on = "SLIP_BUYR_STD_INFO_ID", nomatch = 0]  
     
-    tblList$TRIPS <- merge(tblList$TRIPS,  unique(tblList$MON_DOCS[,"TRIP_ID",drop=F]))
-    tblList$PRO_SPC_INFO <- merge(tblList$PRO_SPC_INFO, unique(tblList$TRIPS[,"TRIP_ID",drop=F]))
-    tblList$PRO_SPC_INFO <- merge(tblList$PRO_SPC_INFO, unique(tblList$MON_DOCS[,"MON_DOC_ID",drop=F]))
-    tblList$PRO_SPC_INFO <- merge(tblList$PRO_SPC_INFO, unique(tblList$LOG_EFRT_STD_INFO[,"LOG_EFRT_STD_INFO_ID",drop=F])) #needed?
+    tblList$TRIPS        <- tblList$TRIPS[unique(tblList$MON_DOCS[, .(TRIP_ID)]), on = "TRIP_ID", nomatch = 0]
+    tblList$PRO_SPC_INFO <- tblList$PRO_SPC_INFO[unique(tblList$TRIPS[, .(TRIP_ID)]), on = "TRIP_ID", nomatch = 0]
+    tblList$PRO_SPC_INFO <- tblList$PRO_SPC_INFO[unique(tblList$MON_DOCS[, .(MON_DOC_ID)]), on = "MON_DOC_ID", nomatch = 0]
+    tblList$PRO_SPC_INFO <- tblList$PRO_SPC_INFO[unique(tblList$LOG_EFRT_STD_INFO[, .(LOG_EFRT_STD_INFO_ID)]), on = "LOG_EFRT_STD_INFO_ID", nomatch = 0] #needed?
     
-    tblList$LOG_EFRT_STD_INFO <-  merge(tblList$LOG_EFRT_STD_INFO,   unique(tblList$MON_DOCS[,"MON_DOC_ID",drop=F]))
-    tblList$LOG_SPC_STD_INFO <- merge(tblList$LOG_SPC_STD_INFO,  unique(tblList$LOG_EFRT_STD_INFO[,"LOG_EFRT_STD_INFO_ID",drop=F]))
+    tblList$LOG_EFRT_STD_INFO <-  tblList$LOG_EFRT_STD_INFO[unique(tblList$MON_DOCS[,.(MON_DOC_ID)]), on = "MON_DOC_ID", nomatch = 0]
+    tblList$LOG_SPC_STD_INFO <- tblList$LOG_SPC_STD_INFO[unique(tblList$LOG_EFRT_STD_INFO[,.(LOG_EFRT_STD_INFO_ID)]), on = "LOG_EFRT_STD_INFO_ID", nomatch = 0]    
     
-    tblList$LOG_EFRT_ENTRD_DETS <- merge(tblList$LOG_EFRT_ENTRD_DETS,  unique(tblList$LOG_SPC_STD_INFO[,"LOG_EFRT_STD_INFO_ID",drop=F]))
-    tblList$MON_DOC_ENTRD_DETS <- merge(tblList$MON_DOC_ENTRD_DETS,  unique(tblList$MON_DOCS[,"MON_DOC_ID",drop=F]))  
-    tblList$LOG_SPC_ENTRD_DETS <- merge(tblList$LOG_SPC_ENTRD_DETS, unique(tblList$LOG_SPC_STD_INFO[,"LOG_SPC_STD_INFO_ID",drop=F]))
     
-    postcnt = sum(sapply(tblList, NROW))
+    tblList$LOG_EFRT_ENTRD_DETS <- tblList$LOG_EFRT_ENTRD_DETS[unique(tblList$LOG_SPC_STD_INFO[,.(LOG_EFRT_STD_INFO_ID)]), on = "LOG_EFRT_STD_INFO_ID", nomatch = 0]
+    tblList$MON_DOC_ENTRD_DETS <- tblList$MON_DOC_ENTRD_DETS[unique(tblList$MON_DOCS[,.(MON_DOC_ID)]), on = "MON_DOC_ID", nomatch = 0]  
+    tblList$LOG_SPC_ENTRD_DETS <- tblList$LOG_SPC_ENTRD_DETS[unique(tblList$LOG_SPC_STD_INFO[,.(LOG_SPC_STD_INFO_ID)]), on = "LOG_SPC_STD_INFO_ID", nomatch = 0]
     
+    postcnt = sum(sapply(tblList, nrow))
     if(postcnt==precnt) {
       LOOPAGAIN=FALSE
-      if (!quiet) print(paste0("FINAL:", postcnt))
     } 
   }
+  tblList <- lapply(tblList, as.data.frame)
   return(tblList)
 }
