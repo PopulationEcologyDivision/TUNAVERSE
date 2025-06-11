5161cba78fb14470c4afd18d9ea9cde5278d1f4b
Mike McMahon <mike.mcmahon@dfo-mpo.gc.ca>
2025-06-11 16:26:25 +0000

    pushes updated documentation

--- inst/exampleRun.R
+++ inst/exampleRun.R
@@ -1,10 +1,10 @@
 cxn <- ROracle::dbConnect(DBI::dbDriver("Oracle"), oracle.username, oracle.password, "PTRAN")
-
+BFT_Data <- prepareData(cxn, force.extract  = F,extract_user = "")
 devtools::load_all()
 source("C:/git/Maritimes/Mar.utils/R/get_data_tables.R")
 source("C:/git/Maritimes/Mar.utils/R/zzz.R")
 
-BFT_Data <- prepareData(cxn, force.extract  = F)
+BFT_Data <- prepareData(cxn, force.extract  = F,extract_user = "", ex)
 
 BFT_LOGS <- BFT_LOGS(BFT_Data)
 library(dplyr)
